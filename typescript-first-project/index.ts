2550e14f9cfa1fe65075e31f6e298fc1ec22f937
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-12-28 09:10:45 -0500

    more ex

--- typescript-first-project/index.ts
+++ typescript-first-project/index.ts
@@ -1,31 +1,12 @@
-type Operation = 'multiply' | 'add' | 'divide';
+import express from 'express';
+const app = express();
 
-type Result = number;
+app.get('/ping', (_req, res) => {
+  res.send('hola ');
+});
 
+const PORT = 3003;
 
-const calculator = (a: number, b: number, op : Operation) : Result =>  {
- switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-  }
-}
-
-console.log(calculator(3,4,'add'))
-
-try {
-  console.log(calculator(1, 0 , 'divide'));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
